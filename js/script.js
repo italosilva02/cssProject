459757aaace0c1aec74a541d9849f11eaf60c3da
italosilva02 <italosilv0101@gmail.com>
2021-12-05 00:21:21 -0300

    Correção de bugs encontrados

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
+
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
-=======
-document.querySelector(".hamburguer").addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle("show-menu")
->>>>>>> a5ac609153474a281f7d0ce83ecdb2b695c00581
 );
